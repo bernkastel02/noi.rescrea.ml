461ce909907e7647d944a95d3eb3adbb69da61b3
Prixyn <17104706+prixyn@users.noreply.github.com>
2017-10-08 01:43:39 -0700

    Create style.js

---
+++ js/style.js
@@ -0,0 +1,43 @@
+var days = 0;
+var hours = 0;
+var minutes = 0;
+var seconds = 0;
+
+setInterval(setTimer, 1000)
+
+function setTimer() {
+    if (hours == 24) {
+        ++days
+        hours = 0
+        minutes = 0
+        seconds = 0
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+    }
+    else if (minutes == 60) {
+        ++hours
+        minutes = 0
+        seconds = 0
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+    }
+    else if (seconds == 60) {
+        ++minutes
+        seconds = 0
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+    }
+    else {
+        ++seconds
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+    }
+}
